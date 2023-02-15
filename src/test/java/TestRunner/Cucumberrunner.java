afe4330b760f313fa48f5960fa5c569e975a3bb5
RashedBarat <rashedbarat@gmail.com>
2023-02-15 23:50:30 +0600

    rashed commit

---
+++ src/test/java/TestRunner/Cucumberrunner.java
@@ -0,0 +1,20 @@
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
+
+public class Cucumberrunner {
+
+    @CucumberOptions(tags ="@smoke", features = {"src/test/resources/Features/login.feature"},
+            glue = {"WebStepsDef"},
+            monochrome = true,
+            dryRun = false,
+            plugin = {
+                    "pretty",
+                    "html:build/reports/feature.html"
+            })
+    @Test
+    public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+    }
+}
